1b2e4606ca7f49156baa5bad17ad4f3a47af7bdd
Serhii Ostrovetskyi <so@rollncode.com>
2019-06-07 16:59:48 +0300

    Data model + get data from url

---
+++ TestImageList/TestImageList/DataModel.swift
@@ -0,0 +1,30 @@
+//
+//  DataModel.swift
+//  TestImageList
+//
+//  Created by Trainee on 6/7/19.
+//  Copyright © 2019 Trainee. All rights reserved.
+//
+
+import Foundation
+
+class DataModel: Codable {
+    var status = false
+    var message: String?
+    var data = usersData()
+}
+
+class usersData: Codable {
+    var users = [user]()
+    var has_more:Bool = false
+}
+
+class user: Codable {
+    var name:String? = ""
+    var image:String? = ""
+    var items = [String?]()
+}
+
+/*class item: Codable {
+    let data = ""
+}*/
--- TestImageList/TestImageList/ViewController.swift
+++ TestImageList/TestImageList/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var CurrentData: DataModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let urlString = URL(string: "http://sd2-hiring.herokuapp.com/api/users?offset=10&limit=10")
+        if let url = urlString {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                
+                if let httpResponce = response as? HTTPURLResponse{
+                    print(httpResponce.statusCode)
+                }
+                
+                if error != nil {
+                    print(error)
+                } else {
+                    if let usableData = data {
+                        print(usableData)
+                        self.CurrentData = self.parse(data: usableData)
+                    }
+                }
+            }
+            task.resume()
+        }
+        
+        
     }
-
+    
+    
+    
+    private func parse(data: Data) -> DataModel?
+    {
+        do
+        {
+            let decoder = JSONDecoder()
+            let result = try decoder.decode(DataModel.self, from: data)
+            return result
+        }
+        catch
+        {
+            print("JSON Error: \(error)")
+            return nil
+        }
+    }
+    
+    
+    
 
 }
 
